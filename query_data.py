839d61ee34b4244450070ed7a9f32b73e9094204
wzh <13879959010@163.com>
2022-04-13 17:26:45 +0800

    选出北向资金持股超过3%的股票

--- query_data.py
+++ query_data.py
@@ -11,13 +11,14 @@ def GetTusharedataToCsv(pro, endtime, path):
     data = {'股票名称':df['name'], '股票代码':df['ts_code']}
     data = pd.DataFrame(data)
     #print(data)
-    for i in data['股票名称']:          #
+    for i in data['股票名称']:          #剔除ST股票
         if 'ST' in i:
             data = data.drop(index = (data.loc[i == data['股票名称']].index))
     print(data)
-    #north = pro.hk_hold(ts_code = '000002.sz')
-    #north = north.loc[north['trade_date'] == '20220412']
-    #print(north)
+    north = pro.hk_hold('trade_date' == '20220412')
+    north = north.loc[north['exchange'] == ('SH' or 'SZ')] 
+    north = north.loc[ north['ratio'] >= 3.0]
+    print(north)
     data.to_csv(path, encoding = 'gbk')
 
 def main():
