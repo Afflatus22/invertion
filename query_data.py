b2fdb96ce0fbb1fbc96a33fff183f23bd3cb60cb
wzh <13879959010@163.com>
2022-08-11 15:55:33 +0800

    增加剔除ST股票

--- query_data.py
+++ query_data.py
@@ -7,20 +7,28 @@ def GetTusharedataToCsv(pro, endtime, path):
     df = pro.daily(**{"ts_code": "","trade_date": endtime,"start_date": 20220101,"end_date": endtime,"offset": "","limit": ""}, fields=["ts_code","close"])
     df = pro.stock_basic(market = '主板')
     print(df)
+    
     #data = df[['name', 'ts_code']]
     data = {'股票名称':df['name'], '股票代码':df['ts_code']}
     data = pd.DataFrame(data)
     #print(data)
-    for i in data['股票名称']:          #剔除ST股票
+    
+    #剔除ST股票
+    for i in data['股票名称']:          
         if 'ST' in i:
             data = data.drop(index = (data.loc[i == data['股票名称']].index))
-    print(data)
+            
+    #获取北向资金持股%5以上的股票
     north = pro.hk_hold('trade_date' == '20220412')
-    north = north.loc[north['exchange'] == ('SH' or 'SZ')] 
-    north = north.loc[ north['ratio'] >= 3.0]
-    print(north)
+    north = north.loc[north['exchange'] == ('SH' or 'SZ')]
+    north = north.loc[ north['ratio'] >= 5.0]
+    for i in data['股票代码']:
+        if  i not in list(north['ts_code']):
+            data = data.drop(index = (data.loc[data['股票代码'] == i].index))
+    print(data)
+    
     data.to_csv(path, encoding = 'gbk')
-
+    
 def main():
     #保存文件路径
     path = 'output.csv'
@@ -40,3 +48,4 @@ def main():
     
 if __name__ == '__main__':
     main()
+    
